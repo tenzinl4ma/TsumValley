e95def2412ba439c8bb946b50623104bce1555b8
M22-007 <m22-007@M22-007-Tenzin.local>
2024-06-26 12:46:27 +0545

    Initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('home.html')
+@app.route('/explore.html')
+def explore():
+    return render_template('explore.html')
+@app.route('/travel.html')
+def travel():
+    return render_template('travel.html')
+@app.route('/news.html')
+def news():
+    return render_template('news.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000)
+    
